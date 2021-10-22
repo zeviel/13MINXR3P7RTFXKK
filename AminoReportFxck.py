48ef62063ee27725ef34aacad913dcb0878b8271
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-10-23 03:37:56 +0600

    Update AminoReportFxck.py

--- AminoReportFxck.py
+++ AminoReportFxck.py
@@ -1,20 +1,20 @@
-import amino
-import asyncio
 import pyfiglet
 from animalsay.utils import say
-from reportconfigs import aminoreportfxckfunctions
+from report_configs import report_functions
 from colored import fore, back, style, attr
 attr(0)
 print(fore.TURQUOISE_2 + style.BOLD)
-print("""Script by Lil Zevi
-Github : https://github.com/LilZevi""")
+print("""Script by deluvsushi
+Github : https://github.com/deluvsushi""")
 print(pyfiglet.figlet_format("AminoReportFxck", font="small"))
 say("cat", """[C] Report Community
 [U] Report User""")
 select = input("Select >> ")
 
 if select == "C":
-	asyncio.get_event_loop().run_until_complete(aminoreportfxckfunctions.report_community())
+	reason = input("Reason >> ")
+	report_functions.report_community(reason=reason)
 	
 elif select == "U":
-	asyncio.get_event_loop().run_until_complete(aminoreportfxckfunctions.report_user())
+	reason = input("Reason >> ")
+	report_functions.report_user(reason=reason)
