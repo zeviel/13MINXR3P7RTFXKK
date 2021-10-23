ac5df72b6d18b34d53915dc9d94347b2f2d80034
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-10-23 10:06:09 +0600

    Update report_functions.py

--- report_configs/report_functions.py
+++ report_configs/report_functions.py
@@ -1,19 +1,16 @@
-import aminofix as amino
-
+import samino
 
 def report_community(reason: str):
-    client = amino.Client()
+    client = samino.Client(None)
     client.login(email=input("Email >> "), password=input("Password >> "))
-    community_info = client.get_from_code(input("Community Link >> "))
+    community_info = client.get_from_link(input("Community Link >> "))
     com_Id = community_info.comId
     while True:
         try:
             client.flag_community(comId=com_Id, reason=reason, flagType=110)
             print("Report Sended")
-        except:
-            print("Report Don't Sended")
-            pass
-
+        except Exception as e:
+        	print(e)
 
 def report_user(reason: str):
     print(
@@ -23,38 +20,33 @@ def report_user(reason: str):
     select = input("Select >> ")
 
     if select == "1":
-        client = amino.Client()
+        client = samino.Client(None)
         client.login(email=input("Email >>"), password=input("Password >> "))
-        user_Info = client.get_from_code(input("User Link >> "))
+        user_Info = client.get_from_link(input("User Link >> "))
         object_Id = user_info.objectId
         com_Id = user_info.comId
-        sub_client = amino.SubClient(comId=com_Id, profile=client.profile)
+        local = samino.Local(comId=com_Id)
         while True:
             try:
-                sub_client.flag(reason=reason, flagType=5, userId=object_Id)
+                local.flag(reason=reason, flagType=5, userId=object_Id)
                 print("Report Sended")
             except:
                 print("Report Don't Sended")
                 pass
 
     elif select == "2":
-        client = amino.Client()
+        client = samino.Client(None)
         emails = open("emails.txt", "r")
         password = input("Password For All Accounts >> ")
-        user_Info = client.get_from_code(input("User Link >> "))
-        object_Id = user_info.objectId
-        com_Id = user_info.comId
+        user_Info = client.get_from_link(input("User Link >> "))
+        object_Id = user_info.objectId; com_Id = user_info.comId
         for line in emails:
             email = line.strip()
             try:
                 client.login(email=email, password=password)
                 client.join_community(com_Id)
-                sub_client = amino.SubClient(comId=com_Id, profile=client.profile)
-                sub_client.flag(reason=reason, flagType=5, userId=object_Id)
+                local = samino.Local(comId=com_Id)
+                local.flag(reason=reason, flagType=5, userId=object_Id)
                 print(f"{email} Sended Report")
-            except amino.lib.util.exceptions.VerificationRequired as e:
-                print(f"Verification required for {email}")
-                link = e.args[0]["url"]
-                print(link)
             except Exception as e:
                 print(e)
