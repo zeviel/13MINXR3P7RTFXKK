0b8efc8c62503fc18376c10519c79f1c62215ba7
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-10-25 21:17:06 +0600

    Update report_functions.py

--- report_configs/report_functions.py
+++ report_configs/report_functions.py
@@ -1,9 +1,9 @@
-import samino
+import aminofix
 
 def report_community(reason: str):
-    client = samino.Client(None)
+    client = aminofix.Client()
     client.login(email=input("Email >> "), password=input("Password >> "))
-    community_info = client.get_from_link(input("Community Link >> "))
+    community_info = client.get_from_code(input("Community Link >> "))
     com_Id = community_info.comId
     while True:
         try:
@@ -20,32 +20,32 @@ def report_user(reason: str):
     select = input("Select >> ")
 
     if select == "1":
-        client = samino.Client(None)
+        client = aminofix.Client()
         client.login(email=input("Email >>"), password=input("Password >> "))
-        user_Info = client.get_from_link(input("User Link >> "))
+        user_Info = client.get_from_code(input("User Link >> "))
         object_Id = user_Info.objectId; com_Id = user_Info.comId
-        local = samino.Local(comId=com_Id)
+        sub_client =  aminofix.SubClient(comId=com_Id, profile=client.profile)
         while True:
             try:
-                local.flag(reason=reason, flagType=5, userId=object_Id)
+                sub_client.flag(reason=reason, flagType=5, userId=object_Id)
                 print("Report Sended")
             except Exception as e:
                 print(e)
                 
 
     elif select == "2":
-        client = samino.Client(None)
+        client = aminofix.Client()
         emails = open("emails.txt", "r")
         password = input("Password For All Accounts >> ")
-        user_Info = client.get_from_link(input("User Link >> "))
+        user_Info = client.get_from_code(input("User Link >> "))
         object_Id = user_Info.objectId; com_Id = user_Info.comId
         for line in emails:
             email = line.strip()
             try:
                 client.login(email=email, password=password)
                 client.join_community(com_Id)
-                local = samino.Local(comId=com_Id)
-                local.flag(reason=reason, flagType=5, userId=object_Id)
+                sub_client =  aminofix.SubClient(comId=com_Id, profile=client.profile)
+                sub_client.flag(reason=reason, flagType=5, userId=object_Id)
                 print(f"{email} Sended Report")
             except Exception as e:
                 print(e)
